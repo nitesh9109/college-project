ee36e91ab09df81913195cede80bdaa78dce94be
nitesh nagar <nitesh.nagar@MeruMesh.com>
2024-04-28 15:07:31 +0530

    server url is added

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://college-project-gq5i.onrender.com",
   }),
   tagTypes: ["User"],
   // eslint-disable-next-line no-unused-vars
